261271224a81ad9b64fa4df5a8981e3c0814e5b6
mambetalieva <116174694+mambetalieva@users.noreply.github.com>
2022-10-24 22:05:22 +0600

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,58 @@
+
+import Foundation
+//Нахождение квадрата числа
+//a * a
+//var a = 2
+//print(a * a)
+
+//func getSquare(num: Int){
+//    print(num * num)
+//}
+//let getSquare = 4
+
+//Нахождение куба числа
+//a * a * a
+
+//let a = 5
+// print(a * a * a)
+
+//func getSguare(num: Int){
+//    print(num * num * num)
+//}
+// var getSquare = 25
+//Нахождение периметра куба
+// a * 8
+//let a = 8
+//print(a + a)
+//func getSquare(string: Int) {
+//   print(string + string)
+//}
+
+
+//Нахождение площади прямоугольника
+//(a * b) * 2
+
+//var a = 2
+//let b = a
+//print(a * b * 2)
+//func getSquare(string: Int) {
+//    print( a * b)
+//}
+//func getSquare(num2: Int) {
+//   print(b * 2)
+//}
+
+//Нахождение площади круга
+//2 * 3.14 * r^2
+var p = 2
+let s = 3.14
+print(2 * 3.14 * 2)
+
+func getSquare(curcle: Int) {
+    print(2 * 3.14 * 2)
+}
+
+
+
+
+
