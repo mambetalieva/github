a368b1dfad16063e1350e2c2e0f13fb60e82bba5
mambetalieva <116174694+mambetalieva@users.noreply.github.com>
2022-11-11 15:22:19 +0600

    7thHomeW

--- main.swift
+++ main.swift
@@ -1,83 +1,61 @@
-//
-//  main.swift
-//  6thlessonHomeW
-//
-//  Created by Каира on 7.11.2022.
-//
+//  Created by Каира on 9.11.2022
 
-//#1. Создать двухмерный массив
-//Первый слой - числа либо строки
-//Второй - элементы
-//Задача: При вводе чисел либо строк в readLine(неограниченное количество) отсортировать и распределить по нужным массивам
+//Воин, Колдун, Медик, Защитник
+//У каждого по 1000 ХП изначально (у защитника 1500)
+//Каждый раунд наносят удар по 200, 300, 0, 100
+//Медик лечит один раз в 2 раунда всю команду на 200 ХП кроме себя самого
+//Медик наносить удар не может
 
-var words2dArray:  [Int] = []
-var char2darray: [Int] = []
-char2darray.sort()
-words2dArray.sort()
-var num2dArray: [Int] = []
-num2dArray.sort()
-print(num2dArray)
-//var StringArray:Array = [""]
-//var namesArray = [ String]()
-//var stop = false
-//while stop == false{
-//    let readLine = readLine()!
-//    if readLine.count == 0{
-//        stop = true
-//    }else{
-//        namesArray.append(readLine)
-//    }
-//}
-//print(namesArray)
-//
-//var names: [String] = ["Ali","Tima","Reka","Sever"]
-//var numbers: [Int] = [1,2,3,4,5,6,7,8,9,15]
-//names.forEach({print($0)})
-//for (index,value) in names.enumerated(){
-//    print("\(index) - \(value)")
-//}
-//
-//for i in 0 ..< numbers.count{
-//    print(numbers[i])
-//}
-//
-//print(namesArray)
-//
+var players = ["Воин","Колдун","Медик","Защитник"]
+var playersXP = [1000,1000,1000,1500]
+var round = 0
+var ss = false
+var punchboss = 0
+var XPboss = 5000
 
-
-
-
-
-
-
-//#2. Создать двухмерный массив
-//Первый слой - числа от 1 до 100
-//Второй слой - рандомные слова в количестве, соответствующему первому слою
-//Задача: Создать массив с разными словами и в цикле добавить рандомно слова в количестве первого слоя в массив по текущему номеру
-
-var array: [Int] = [1,2,3,4,5]
-array.append(40)
-array.append(41)
-array.append(42)
-array.append(80)
-
-print(array)
-
-var dictionary: [String: Int] = ["keep":1,"brother":2,"lady":3,"smart":4]
-dictionary["keep"]
-dictionary["brother"]
-dictionary["lady"]
-dictionary["smart"] = nil
-print(dictionary)
-
-
-
-//
-//#3. Выполнить 5 задач из раздела 8kyu на сайте codewars.com
-//Задачи выбираете сами
-//
-//Критерии приемки:
-//Код каждого студента должен быть уникальным
-//Весь проект должен находиться в github (Все решения задач должны находиться в одном проекте так-же в коде должны комментарии которые доказывают работоспособность вашего кода)
-//
-//
+while playersXP[0]>0 || playersXP[1] > 0 || playersXP[2] > 0 || playersXP[3] > 0{
+    round += 1
+    print("round:\(round)")
+    print("ITS TIME TO PLAYERS")
+    if playersXP[0] > 0{
+        XPboss -= 200}else{XPboss -= 0}
+    if playersXP[1] > 0{
+        XPboss -= 300}else{XPboss -= 0}
+    if playersXP[3] > 0{
+        XPboss -= 100}else{XPboss -= 0}
+    
+    print ("ITS THE TIME TO BOSS")
+    if playersXP[0] > 0{
+        playersXP[0] -= 200}
+    if playersXP[1] > 0{
+        playersXP[1] -= 200}
+    if playersXP[2] > 0{
+        playersXP[2] -= 200}
+    if playersXP[3] > 0{
+        playersXP[3] -= 200}
+    if playersXP[2] > 0{
+        if round % 2 == 0{
+            print("ITS TIME TO MEDIC")
+        }
+        
+        if playersXP[2] > 0{
+            if round % 2 == 0{
+                playersXP[0] += 200
+                playersXP[1] += 200
+                playersXP[3] += 200
+            }
+        }}
+    print("playersXP:\(playersXP)")
+    if XPboss == 0{
+        print("THE BOSS LOST")
+        ss = true
+    }
+    for item in 0...players.count - 1{
+        if playersXP[item] <= 0{
+            print("THE PLAYERS LOST")
+            ss = true
+            
+        }
+    }}
+print("GAME OVER")
+print("ROUNDS:\(round)")
