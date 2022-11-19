4a64808b359aa973a568cd4b28e41db34d2f8e26
mambetalieva <116174694+mambetalieva@users.noreply.github.com>
2022-11-19 17:02:19 +0600

    Add files via upload
    home work 19.11.22

--- main.swift
+++ main.swift
@@ -1,61 +1,163 @@
-//  Created by Каира on 9.11.2022
+import Foundation
+//1.Задание Решение
+//
+//class Car{
+//    var name: String
+//    var color: String
+//    var model: String
+//    var speed: Int
+//    func update(name:String, color:String,model: String,speed: Int){
+//        print("\(name),\(color),\(model),\(speed)")
+//    }
+//
+//    init(name: String, color: String, model: String, speed: Int) {
+//        self.name = name
+//        self.color = color
+//        self.model = model
+//        self.speed = speed
+//        }
+//    func printInfo(){
+//        print("\(name),\(color),\(model),\(speed)")
+//        print("Улучшение")
+//        update(name: "Mercedes", color: "black", model: "220", speed: 240)
+//    }
+//}
+//var car = Car(name: "Mercedes", color: "black", model: "210", speed: 220)
+//car.printInfo()
+//
+//2. Создать класс "Магазин" и "Продукт" и задать им свойства
+//Также создать функцию добавления продукта в магазин и отображения товаров в магазине
+//class Narodnyi{
+//    var name: String
+//    var products: [ Producty] = [Producty]()
+//    init(name: String) {
+//        self.name = name
+//    }
+//    func printInfo(){
+//        print(name)
+//
+//        for lisst in products{
+//            print("producty - \(lisst.name) cost - \(lisst.price) count - \(lisst.count)")
+//        }
+//    }
+//    func products(lisst:Producty){
+//        products.append(lisst)
+//    }
+//}
+//var Shop = Narodnyi(name: "Narodnyi")
+//Shop.printInfo()
+//class Producty{
+//    var name: String
+//    var price: Int
+//    var count: Int
+//    init(name: String, price: Int, count: Int) {
+//        self.name = name
+//        self.price = price
+//        self.count = count
+//    }
+//    func printInfo(){
+//        print("\(name),\(price),\(count)")
+//    }
+//}
+//var  bounty = Producty(name: "bounty", price: 100, count: 200 )
+//var skitlss = Producty(name:"skitlss", price: 85, count: 36)
+//var sugar = Producty(name: "sugar", price: 82, count: 48)
+//var milk = Producty(name: "milk", price: 65, count: 72)
+//var sweets = Producty(name: "sweets", price: 650, count: 4)
+//Shop.products(lisst: bounty)
+//Shop.products(lisst: skitlss)
+//Shop.products(lisst: sugar)
+//Shop.products(lisst: milk)
+//Shop.products(lisst: sweets)
+//Shop.printInfo()
 
-//Воин, Колдун, Медик, Защитник
-//У каждого по 1000 ХП изначально (у защитника 1500)
-//Каждый раунд наносят удар по 200, 300, 0, 100
-//Медик лечит один раз в 2 раунда всю команду на 200 ХП кроме себя самого
-//Медик наносить удар не может
-
-var players = ["Воин","Колдун","Медик","Защитник"]
-var playersXP = [1000,1000,1000,1500]
-var round = 0
-var ss = false
-var punchboss = 0
-var XPboss = 5000
-
-while playersXP[0]>0 || playersXP[1] > 0 || playersXP[2] > 0 || playersXP[3] > 0{
-    round += 1
-    print("round:\(round)")
-    print("ITS TIME TO PLAYERS")
-    if playersXP[0] > 0{
-        XPboss -= 200}else{XPboss -= 0}
-    if playersXP[1] > 0{
-        XPboss -= 300}else{XPboss -= 0}
-    if playersXP[3] > 0{
-        XPboss -= 100}else{XPboss -= 0}
-    
-    print ("ITS THE TIME TO BOSS")
-    if playersXP[0] > 0{
-        playersXP[0] -= 200}
-    if playersXP[1] > 0{
-        playersXP[1] -= 200}
-    if playersXP[2] > 0{
-        playersXP[2] -= 200}
-    if playersXP[3] > 0{
-        playersXP[3] -= 200}
-    if playersXP[2] > 0{
-        if round % 2 == 0{
-            print("ITS TIME TO MEDIC")
-        }
-        
-        if playersXP[2] > 0{
-            if round % 2 == 0{
-                playersXP[0] += 200
-                playersXP[1] += 200
-                playersXP[3] += 200
-            }
-        }}
-    print("playersXP:\(playersXP)")
-    if XPboss == 0{
-        print("THE BOSS LOST")
-        ss = true
+//3. Создать класс "Банк" и "Клиент"
+//создать функции для:
+//1) Подсчета клиентов банка
+//2) Отображения баланса по ID или фамилии клиента
+//3) Перевода средств от клиента к другому клиенту
+class Bank{
+    var name: String
+    var currency: String
+    var client:[Customer] = [Customer]()
+    init(name: String, currency: String) {
+        self.name = name
+        self.currency = currency
     }
-    for item in 0...players.count - 1{
-        if playersXP[item] <= 0{
-            print("THE PLAYERS LOST")
-            ss = true
+    func bank(){
+        print("Банк - \(name), валюта \(currency)")
+        
+        for client1 in client {
+            print("customer - \(client1.name) ID - \(client1.ID) wallet - \(client1.wallet) transfer - \(client1.transfer)")
+        }
+    }
+    func men(clients:Customer){
+        client.append(clients)
+    }
+}
+    var vank = Bank(name: "Optima", currency: "сом")
+vank.bank()
+
+    class Customer{
+        var name: String
+        var ID: Int
+        var wallet: Int
+        var transfer: Int
+        init(name: String, ID: Int, wallet: Int, transfer: Int) {
+            self.name = name
+            self.ID = ID
+            self.wallet = wallet
+            self.transfer = transfer
             
         }
-    }}
-print("GAME OVER")
-print("ROUNDS:\(round)")
+
+        func printInfo(){
+            print("name - \(name), ID - \(ID), wallet - \(wallet), transfer - \(transfer)")
+        }
+    }
+    var base = Customer(name: "Ted", ID: 7788521, wallet: 200000, transfer: 40000)
+base.printInfo()
+
+
+
+
+
+
+
+
+//Критерии приемки:
+//Код каждого студента должен быть уникальным
+//Весь проект должен находиться в github (Все решения задач должны находиться в одном проекте так-же в коде должны комментарии которые доказывают работоспособность вашего кода)
+
+
+
+
+
+// dop.
+
+//class Human{
+//    var name: String
+//    var age: Int
+//    var country: String
+//    var city: String
+//    init(name: String, age: Int, country: String, city: String){
+//        self.name = name
+//        self.age = age
+//        self.country = country
+//        self.city = city
+//    }
+//    func printInfo(){
+//        print("My name is - \(name)\nIm - \(age)\nIm from  - \(country)\nI live in - \(city)")
+//    }
+//}
+//var human: Human? = Human(name:"Tina", age: 15, country: "Kyrgyzstan", city: "Kant")
+//var person: Human? = Human(name:"Elya", age: 28, country: "Iran", city: "Tebriz")
+//var person1:Human? = Human(name: "Melis", age: 25, country: "Turkiye", city: "Izmir")
+//var person2: Human? = Human(name: "Tanya", age: 36, country: "Russia", city: "Beloyarsky")
+//var person3: Human? = Human(name:"Nuri" , age: 19, country: "Kazakhstan", city: "Karaganda")
+//human?.printInfo()
+//person?.printInfo()
+//person1?.printInfo()
+//person2?.printInfo()
+//person3?.printInfo()
+//
